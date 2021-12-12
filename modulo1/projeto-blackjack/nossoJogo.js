8bebe53a8bddcd483a08a01c579e73eb40b8fcdd
ElRafa002 <rafasalmeronjr@gmail.com>
2021-12-12 17:05:31 -0300

    NossoJogo.js funcionando

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -12,3 +12,27 @@
  */
 
 console.log("Boas vindas ao jogo de Blackjack!");
+
+if (confirm("Quer  iniciar uma nova rodada?")){
+   const carta_usuario_1 = comprarCarta();
+   const carta_usuario_2 = comprarCarta();
+   
+   const carta_computador_1 = comprarCarta();
+   const carta_computador_2 = comprarCarta();
+
+   const pontos_usuario = carta_usuario_1.valor + carta_usuario_2.valor;
+   const pontos_computador = carta_computador_1.valor + carta_computador_2.valor;
+   
+   console.log(`Cartas do Usuário: ${carta_usuario_1.texto} ${carta_usuario_2.texto} - ${pontos_usuario}`);
+   console.log(`Cartas do Computador: ${carta_computador_1.texto} ${carta_computador_2.texto} - ${pontos_computador}`);
+   if (pontos_usuario > pontos_computador){
+      console.log("O Usuário Venceu!");
+   } else if (pontos_computador > pontos_usuario){
+      console.log("O Computador Venceu!");
+   } else if (pontos_usuario === pontos_computador){
+      console.log("Deu empate!");
+   }
+}  else {
+   console.log("O Jogo Terminou.")
+};
+   
