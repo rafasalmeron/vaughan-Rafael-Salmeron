2a32ebcd4cb38104ec9c039060d90d2d7c2c6200
ElRafa002 <rafasalmeronjr@gmail.com>
2022-01-25 14:30:10 -0300

    FirstStep

---
+++ modulo2/apis-labeusers/my-app/src/App.js
@@ -0,0 +1,58 @@
+import react from "react";
+import axios from "axios";
+
+class initialPage extends react.Component {
+  state = {
+    name: "",
+    email: "",
+  };
+
+  handleName = (event) => {
+    this.setState({ name: event.target.value });
+  };
+  handleEmail = (e) => {
+    this.setState({ email: e.target.value });
+  };
+  handleCreateUser = () => {
+    const axiosConfig = {
+      headers: { Authorization: "rafael-salmeron-vaughan" },
+    };
+    const body = {
+      name: this.state.name,
+      email: this.state.email,
+    };
+    axios.post(
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+      body,
+      axiosConfig
+    )
+    .then(() => {
+      alert(`Usuário ${this.getSnapshotBeforeUpdate.name} Criado com Sucesso!`);
+      this.setState({name:"", email: ""});
+    })
+    .catch(error => {
+      alert("Falha ao criar usuário.")
+      console.log(error)
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          placeholder="Nome"
+          value={this.state.name}
+          onChange={this.handleName}
+        />
+        <input
+          placeholder="e-mail"
+          value={this.state.email}
+          onChange={this.handleEmail}
+        />
+        <button onClick={this.handleCreateUser} >Criar Usuário</button>
+      </div>
+    );
+  }d
+}
+
+export default initialPage;
