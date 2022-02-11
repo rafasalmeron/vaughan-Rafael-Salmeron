4f7ce579a5ba4d6ddf9c960f3ab7d8bf652d3390
ElRafa002 <rafasalmeronjr@gmail.com>
2022-02-11 17:02:49 -0300

    Começando a preparar as coisas

--- modulo2/astromatch/src/App.js
+++ modulo2/astromatch/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import Perfil from './components/Perfil/Perfil'
-function App() {
+import Bottom from './components/Bottom/Bottom'
+import styled from 'styled-components';
 
+const Conteiner = styled.main`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+  border: 1px solid #000;
+  height: 70%;
+`
+
+function App() {
   return (
-      <div className='page'>
-      <Perfil/>
-      </div>
+      <body>
+      <Conteiner>
+        <header>
+          Astromatch
+        </header>
+        <Perfil/>
+        <section>
+          <Bottom />
+        </section>
+      </Conteiner>
+      </body>
   );
 }
-
 export default App;
