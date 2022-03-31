eb79e533ba75758b0921d69d71a745dc0af9c605
ElRafa002 <rafasalmeronjr@gmail.com>
2022-03-31 08:12:03 -0300

    1.00

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -1,30 +1,56 @@
-import express from "express";
-import cors from "cors";
-import { users } from './data';
+import express from 'express';
+import cors from 'cors';
+import { products } from './data';
 
 
 const app = express();
-app.use(cors);
 app.use(express.json());
+app.use(cors());
 
 
-
-app.get("/playlists", (req, res) => {
-    const allUsers = users;
-
-    const playlists = allUsers.map((user) => {
-        return user.playlists;
-    });
-
-    const resultPlaylists = playlists.flat(1) // flat(profundidade da busca)
-    // [1,[2,3], 4] -> flat(1) -> [1,2,3,4]
-
-    res.status(200).send(resultPlaylists);
+app.get("/", (req, res) => {
+    res.status(200).send(products)
 });
 
+app.post('/', (req, res) => {
+    const  id = Math.floor(Date.now() * Math.random()).toString(36)
+    const  name = req.body.name
+    const  price = req.body.price
+    
+    const newProducts = {
+        id: id,
+        name: name,
+        price: price
+    }
+    products.push(newProducts)
+    
+    res.status(200).send(products)
+})
 
+app.put('/:id', (req, res) => {
+    const newPrice = req.body.price
+    const id = req.params.id
+    
+    products.forEach((p) => {
+        if(id === p.id) {
+            p.price = newPrice
+        }
+    })
+    res.status(200).send(products)
+})
+
+app.delete('/:id', (req, res) => {
+    const id: any = req.params.id
+
+    const newProducts = products.map((p) => {
+        if(!p.id[id]){
+            return 'error'
+        } else {}
+    })
+    res.status(200).send(newProducts)
+})
 
 
 app.listen(3003, () => {
-    console.log("Backend rodando na porta 3003!");
+    console.log("Server rodando na porta 3003!");
 });
