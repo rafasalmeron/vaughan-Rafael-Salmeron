b0ed1b734c8ba396245246a6e2d4ca5c17bf7f50
rafasalmeron <rafasalmeronjr@gmail.com>
2022-04-06 21:59:39 -0300

    Aula Knex

---
+++ modulo4/knex-js/src/connection.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import knex from "knex";
+
+dotenv.config()
+
+const connection = knex({
+    client: "mysql",
+    connection:{
+        host: process.env.DB_HOST,
+        port:3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        multiStatements: true
+    }
+})
+export default connection 
